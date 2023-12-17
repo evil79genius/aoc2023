54bdc1087796eb1a93821f7d1000cdd598f5ae89
Carlo Ferretti <evil79genius@gmail.com>
2023-12-17 21:00:17 +0100

    05

---
+++ 05A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _05A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass input filename as 1st parameter");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+
+            long[] source = lines[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => long.Parse(s)).ToArray();
+
+            int curLine = 1;
+            while (curLine < lines.Length)
+            {
+                long[] dest = (long[])source.Clone();
+                while (++curLine < lines.Length)
+                {
+                    if (lines[curLine].EndsWith("map:")) continue;
+                    if (lines[curLine].Length == 0) break;
+                    long[] map = lines[curLine].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => long.Parse(s)).ToArray();
+                    for (int s = 0; s < source.Length; s++)
+                    {
+                        if (source[s] >= map[1] && source[s] < (map[1] + map[2]))
+                        {
+                            dest[s] = source[s] - map[1] + map[0];
+                        }
+                    }
+                }
+                source = dest;
+            }
+            Console.WriteLine("{0}", source.Min());
+        }
+    }
+}
---
+++ 05B/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _05B
+{
+    internal class Program
+    {
+        private class Map
+        {
+            public long To;
+            public long From;
+            public long Range;
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass input filename as 1st parameter");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+
+            string sources = lines[0].Split(':')[1];
+            List<(long, long)> source = new Regex(@"([\d]+) ([\d]+)").Matches(sources).Select(m => (long.Parse(m.Groups[1].Value), long.Parse(m.Groups[2].Value))).ToList();
+
+            int curLine = 1;
+            while (curLine < lines.Length)
+            {
+                List<Map> map = new List<Map>();
+                while (++curLine < lines.Length)
+                {
+                    if (lines[curLine].EndsWith("map:")) continue;
+                    if (lines[curLine].Length == 0) break;
+                    long[] mapLine = lines[curLine].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => long.Parse(s)).ToArray();
+                    map.Add(new Map {
+                        To = mapLine[0],
+                        From = mapLine[1],
+                        Range = mapLine[2]
+                    });
+                }
+                List<(long, long)> dest = new List<(long, long)>();
+                while (source.Any())
+                {
+                    (long start, long range) = source.First();
+                    source.RemoveAt(0);
+                    while (range > 0)
+                    {
+                        Map? entry = map.Where(m => m.From < (start + range) && (m.From + m.Range) > start).OrderBy(m => m.From).FirstOrDefault();
+                        if (entry != null)
+                        {
+                            long mapTo = entry.To;
+                            long mapFrom = entry.From;
+                            long mapRange = entry.Range;
+                            if (start < mapFrom)
+                            {
+                                // Unmapped portion at the beginning
+                                dest.Add((start, mapFrom - start));
+                                range -= mapFrom - start;
+                                start = mapFrom;
+                            }
+                            if (mapFrom < start)
+                            {
+                                // Move mapping ahead
+                                mapRange -= start - mapFrom;
+                                mapTo += start - mapFrom;
+                            }
+                            if (mapRange < range)
+                            {
+                                // Not enough to map whole range
+                                dest.Add((mapTo, mapRange));
+                                start += mapRange;
+                                range -= mapRange;
+                            }
+                            else
+                            {
+                                // Map whole range
+                                dest.Add((mapTo, range));
+                                range = 0;
+                            }
+                        }
+                        else
+                        {
+                            // No overlapping map entry
+                            dest.Add((start, range));
+                            range = 0;
+                        }
+                    }
+                }
+                source = dest;
+            }
+            Console.WriteLine("{0}", source.Select(s => s.Item1).Min());
+        }
+    }
+}

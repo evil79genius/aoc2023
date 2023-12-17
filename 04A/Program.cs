47390c5b0b585d0fbbbf0b4199c7550581da9635
Carlo Ferretti <evil79genius@gmail.com>
2023-12-17 19:11:55 +0100

    04

---
+++ 04A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _04A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass input filename as 1st parameter");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+            Regex card = new Regex(@"(.*):(.*)\|(.*)");
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                var match = card.Match(line);
+                var winning = match.Groups[2].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToHashSet();
+                var owned = match.Groups[3].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToHashSet();
+                int score = winning.Count(i => owned.Contains(i));
+                if (score > 0)
+                {
+                    sum += (int)Math.Pow(2, (score - 1));
+                }
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
---
+++ 04B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _04B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass input filename as 1st parameter");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+            Regex card = new Regex(@"(.*):(.*)\|(.*)");
+            int sum = 0;
+            int[] copies = lines.Select(l => 1).ToArray();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var match = card.Match(lines[i]);
+                var winning = match.Groups[2].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToHashSet();
+                var owned = match.Groups[3].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToHashSet();
+                int score = winning.Count(i => owned.Contains(i));
+                for (int j = i + 1; j <= i + score; j++)
+                {
+                    if (j < lines.Length)
+                    {
+                        copies[j] += copies[i];
+                    }
+                }
+                sum += copies[i];
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}

69a93d9140baf3e802213dffdf3c302aa44c3e05
Carlo Ferretti <evil79genius@gmail.com>
2023-12-17 16:44:44 +0100

    01

---
+++ 01A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass input filename as 1st parameter");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+            int sum = 0;
+            Regex digit = new Regex("[0-9]");
+            foreach (string line in lines)
+            {
+                var mc = digit.Matches(line);
+                sum += (Convert.ToInt32(mc.First().Value) * 10) + Convert.ToInt32(mc.Last().Value);
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
---
+++ 01B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01B
+{
+    enum Numbers {
+        zero = 0, one = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9
+    };
+
+    internal class Program
+    {
+        static int Parse(string number)
+        {
+            int result = 0;
+            if (Int32.TryParse(number, out result)) return result;
+            return (int)Enum.Parse<Numbers>(number);
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass input filename as 1st parameter");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+            int sum = 0;
+            Regex digit = new Regex("[0-9]|" + string.Join('|', Enum.GetValues<Numbers>()));
+            foreach (string line in lines)
+            {
+                var mc = digit.Matches(line);
+                int value = (Parse(mc.First().Value) * 10) + Parse(mc.Last().Value);
+                sum += value;
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}

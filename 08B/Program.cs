930201a6ac718533402e2ea8c261267809078440
Carlo Ferretti <evil79genius@gmail.com>
2023-12-18 23:36:16 +0100

    08B (Almost perfect)

--- 08B/Program.cs
+++ 08B/Program.cs
@@ -1,9 +1,21 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime;
+using System.Text.RegularExpressions;
 
 namespace _08B
 {
     internal class Program
     {
+        private static long Gcd(long a, long b)
+        {
+            long r;
+            while ((r = a % b) != 0)
+            {
+                a = b;
+                b = r;
+            }
+            return b;
+        }
+
         static void Main(string[] args)
         {
             if (args.Length == 0)
@@ -17,24 +29,52 @@ namespace _08B
             Regex node = new Regex(@"[12A-Z]{3}");
             Dictionary<string, (string, string)> map = lines[2..].Select(l => node.Matches(l)).ToDictionary(m => m[0].Value, m => (m[1].Value, m[2].Value));
 
-            string[] curNodes = map.Keys.Where(k => k.EndsWith('A')).ToArray();
-            int step = 0;
-            int sub = 0;
-            while (!curNodes.All(n => n.EndsWith('Z')))
+            string[] startNodes = map.Keys.Where(k => k.EndsWith('A')).ToArray();
+            var loops = startNodes.Select(s => KeyValuePair.Create(s,
+                new Dictionary<string, List<long>>()
+                )).ToDictionary();
+            Dictionary<string, long> start = new Dictionary<string, long>();
+            Dictionary<string, long> periods = new Dictionary<string, long>();
+            foreach (string startNode in startNodes)
             {
-                curNodes = curNodes.Select(n => path[sub] == 'L'
-                    ? map[n].Item1
-                    : map[n].Item2).ToArray();
-                int cnt = curNodes.Count(n => n.EndsWith('Z'));
-                if (cnt > 2)
+                string curNode = startNode;
+                long step = 0;
+                long? prev = null;
+                while(true)
                 {
-                    Console.WriteLine("Step {0}[{1}] - {2}: {3}", step, sub, cnt, string.Join(' ', curNodes));
+                    curNode = path[step % path.Length] == 'L'
+                        ? map[curNode].Item1
+                        : map[curNode].Item2;
+                    step++;
+                    if (curNode.EndsWith('Z'))
+                    {
+                        if (!loops[startNode].ContainsKey(curNode))
+                        {
+                            loops[startNode][curNode] = new List<long>();
+                        }
+                        prev = loops[startNode][curNode].Select(s => (long?)s).FirstOrDefault(s => ((step - s) % path.Length) == 0);
+                        if (prev.HasValue)
+                        {
+                            periods[startNode] = step - prev.Value;
+                            loops[startNode][curNode].Add(step);
+                            break;
+                        }
+                        loops[startNode][curNode].Add(step);
+                    }
                 }
-                step++;
-                sub++;
-                if (sub == path.Length) { sub = 0; }
+                Console.WriteLine("Period: {0}\nzNodes: {1}", step - prev, string.Join(' ', loops[startNode].SelectMany(kvp => kvp.Value).ToList()));
             }
-            Console.WriteLine("{0}", step);
+            Console.WriteLine("\nPeriods: {0}", string.Join(' ', periods.Values));
+            long lcm = 1;
+            while (periods.Count > 0)
+            {
+                string key = periods.Keys.First();
+                long fact = periods[key];
+                periods.Remove(key);
+                long gcd = Gcd(lcm, fact);
+                lcm = lcm / gcd * fact;
+            }
+            Console.WriteLine("{0}", lcm);
         }
     }
 }
